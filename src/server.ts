137bd6eedec02fef48bbb6991a98bed78f56a579
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2024-05-23 11:48:18 +0600

    second commit

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
+// import mongoose from "mongoose";
+// import app from "./app";
+// import config from "./app/config";
+
+// async function main() {
+//   try {
+//     await mongoose.connect(config.database_url as string);
+
+//     app.listen(config.port, () => {
+//       console.log(`App is listening on port ${config.port}`);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// main();
+
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
@@ -5,12 +23,14 @@ import config from "./app/config";
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
+    console.log("Connected to MongoDB");
 
     app.listen(config.port, () => {
       console.log(`App is listening on port ${config.port}`);
     });
   } catch (err) {
-    console.log(err);
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1); // Exit the process with a failure code
   }
 }
 
